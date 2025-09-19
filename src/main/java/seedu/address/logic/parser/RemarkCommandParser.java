c0b995c95f3dc1e4a40602d1709543c07057f62f
Wen-Cong <wencong.dev@gmail.com>
2025-09-19 17:25:25 +0800

    Refactor code to comply with check style

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -10,6 +10,9 @@ import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Remark;
 
+/**
+ * Parses user input for the 'remark' command.
+ */
 public class RemarkCommandParser implements Parser {
 
     /**
